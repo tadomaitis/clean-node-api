ea6294d89dea3412c7169066b2ae11872e0b2bea
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2020-07-25 18:28:29 -0300

    Add basic test

---
+++ src/presentation/routers/login-router.spec.js
@@ -0,0 +1,18 @@
+class LoginRouter {
+  route () {
+
+  }
+}
+
+describe('Login Router', () => {
+  test('Should return 400 if no email is provided', () => {
+    const sut = new LoginRouter()
+    const httpRequest = {
+      body: {
+        password: 'any_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})
