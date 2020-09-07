770f35bf6c4c580581a7f3664d690c4d7bbcd00c
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2020-09-07 12:42:20 -0300

    Ensure AuthUseCase returns an accessToken if valid credentials are provided

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -118,9 +118,17 @@ describe('Auth UseCase', () => {
       loadUserByEmailRepositorySpy.user.password
     )
   })
+
   test('Should call TokenGenerator with correct userId', async () => {
     const { sut, loadUserByEmailRepositorySpy, tokenGeneratorSpy } = makeSut()
     await sut.auth('valid_email@mail.com', 'valid_password')
     expect(tokenGeneratorSpy.userId).toBe(loadUserByEmailRepositorySpy.user.id)
+  })
+
+  test('Should return an accessToken if correct credentials are provided', async () => {
+    const { sut, tokenGeneratorSpy } = makeSut()
+    const accessToken = await sut.auth('valid_email@mail.com', 'valid_password')
+    expect(accessToken).toBeTruthy()
+    expect(accessToken).toBe(tokenGeneratorSpy.accessToken)
   })
 })
