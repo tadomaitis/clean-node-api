600cb27cf3631cedae9cf6c5d5c564d86bddea03
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2020-09-14 08:15:07 -0300

    fix mongo-helper file name

--- src/infra/repositories/load-user-by-email-repository.spec.js
+++ src/infra/repositories/load-user-by-email-repository.spec.js
@@ -1,4 +1,4 @@
-const MongoHelper = require('../helpers/mongo-helpers')
+const MongoHelper = require('../helpers/mongo-helper')
 const LoadUserByEmailRepository = require('./load-user-by-email-repository')
 const MissingParamamError = require('../../utils/errors/missing-param-error')
 let db
