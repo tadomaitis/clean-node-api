f137b0225855c3aa30851dc0ef4dbba11ddd4ef7
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2020-09-15 08:43:38 -0300

    enable CORS

--- src/main/config/setup.test.js
+++ src/main/config/setup.test.js
@@ -9,4 +9,14 @@ describe('App Setup', () => {
     const res = await request(app).get('/test_x_powered_by')
     expect(res.headers['x-powered-by']).toBeUndefined()
   })
+
+  test('Should enable CORS', async () => {
+    app.get('/test_cors', (_, res) => {
+      res.send('ok')
+    })
+    const res = await request(app).get('/testcors')
+    expect(res.headers['access-control-allow-origin']).toBe('*')
+    expect(res.headers['access-control-allow-methods']).toBe('*')
+    expect(res.headers['access-control-allow-headers']).toBe('*')
+  })
 })
