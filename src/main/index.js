d4c520190b1e328eefe9dc5e305453ab9a450536
Thiago Adomaitis <t.d.adomaitis@gmail.com>
2020-09-16 10:51:35 -0300

    connect to MongoDB before listen for app

--- src/main/index.js
+++ src/main/index.js
@@ -1,5 +1,16 @@
-const app = require('./config/app')
+require('dotenv').config()
 
-app.listen(3333, () => {
-  console.log('Server Running on port 3333...')
-})
+const MongoHelper = require('../infra/helpers/mongo-helper')
+
+const port = process.env.PORT
+const db = process.env.MONGODB
+
+MongoHelper.connect(db)
+  .then(() => {
+    const app = require('./config/app')
+
+    app.listen(port, () => {
+      console.log(`Server Running on port ${port}...`)
+    })
+  })
+  .catch(console.error)
